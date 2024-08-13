e81c88a29930e0e82532bd2d8187962367caaa7d
shrfrn <shrfrn@gmail.com>
2024-08-13 12:16:27 +0300

    Set up basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import { Route, HashRouter as Router, Routes } from 'react-router-dom'
+
 import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { RobotIndex } from './pages/RobotIndex'
 
 import { AppFooter } from './cmps/AppFooter'
 import { AppHeader } from './cmps/AppHeader'
 
 export function App() {
-    return <>
+    return <Router>
         <AppHeader />
         <main>
-            <Home />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/robot" element={<RobotIndex />} />
+            </Routes>
         </main>
         <AppFooter />
-    </>
+    </Router>
 }
--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,5 +1,12 @@
+import { NavLink } from "react-router-dom";
+
 export function AppHeader() {
     return <header className="app-header">
         <h1>Robots</h1>
+        <nav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/robot">Robots</NavLink>
+        </nav>
     </header>
 }
